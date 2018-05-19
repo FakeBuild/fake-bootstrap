e03c16d7a22248133add87a20fb416788e09b76d
Juergen Hoetzel <juergen@archlinux.org>
2018-05-19 18:31:00 +0200

    Improve error handling
    cURL download or bash execution might fail

--- fake.sh
+++ fake.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e -o pipefail
+
 # Install .NET Core (https://docs.microsoft.com/en-us/dotnet/core/tools/dotnet-install-script)
 curl -sSL https://dot.net/v1/dotnet-install.sh | bash /dev/stdin --channel Current
 
