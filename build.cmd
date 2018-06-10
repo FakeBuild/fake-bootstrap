8b20cf023e092647cb0b5d048a094f67e812f030
Matthias Dittrich <matthi.d@gmail.com>
2018-06-10 22:04:24 +0200

    Update for fake 5 release

--- build.cmd
+++ build.cmd
@@ -5,8 +5,7 @@ SET BUILD_PACKAGES=BuildPackages
 IF NOT EXIST "%BUILD_PACKAGES%\fake.exe" (
   dotnet tool install fake-cli ^
     --tool-path ./%BUILD_PACKAGES% ^
-    --source-feed https://www.myget.org/F/fake-vsts/api/v3/index.json ^
-    --version 5.0.0-rc*
+    --version 5.*
 )
 
 REM comments following lines once you are done with your script, the idea is to be sure paket install regenerate the lock file if we add new nuget in the fsx
